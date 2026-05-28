0ec7aad38f0c43c6e68f1c32eac4e433bb4c7e17
iTestor <91094276+iTestor@users.noreply.github.com>
2026-05-28 18:57:14 +0200

    Language: Add French

---
+++ Scripts/Translations/fr.lua
@@ -0,0 +1,115 @@
+local Translation = {
+    ModInfo = {
+        Display = "Capacity & QuickBar Tweaks"
+    },
+
+    Debug = {
+        Title = "DÉBOGAGE",
+        Description = "Active les journaux de la console de débogage"
+    },
+
+    QuickBar = {
+        Title = {
+            Toggle = "BARRE RAPIDE",
+            Slots = "Emplacements de la Barre Rapide",
+        },
+        Description = {
+            Toggle =
+            "Active ou désactive les emplacements personnalisés de la Barre Rapide. Lorsqu'ils sont désactivés, la Barre Rapide revient au nombre d'emplacements par défaut du jeu.",
+            Slots = "Nombre d'emplacements à activer dans la Barre Rapide (1-10)",
+        }
+    },
+
+    FloorLocker = {
+        Title = {
+            Toggle = "ARMOIRES",
+            Rows = " Lignes",
+            Cols = " Colonnes"
+        },
+        Description = {
+            Toggle = "Active la capacité de stockage personnalisée des Armoires",
+            Rows = "Nombre de lignes à activer dans les Armoires",
+            Cols = "Nombre de colonnes à activer dans les Armoires"
+        }
+    },
+
+    WallLocker = {
+        Title = {
+            Toggle = "CASIERS MURAUX",
+            Rows = " Lignes",
+            Cols = " Colonnes"
+        },
+        Description = {
+            Toggle = "Active la capacité de stockage personnalisée des Casiers Muraux",
+            Rows = "Nombre de lignes à activer dans les Casiers Muraux",
+            Cols = "Nombre de colonnes à activer dans les Casiers Muraux"
+        }
+    },
+
+    TailingChest = {
+        Title = {
+            Toggle = "COFFRES SCORIES",
+            Rows = " Lignes",
+            Cols = " Colonnes"
+        },
+        Description = {
+            Toggle = "Active la capacité de stockage personnalisée des Coffres Scories",
+            Rows = "Nombre de lignes à activer dans les Coffres Scories",
+            Cols = "Nombre de colonnes à activer dans les Coffres Scories"
+        }
+    },
+
+    FloatingLocker = {
+        Title = {
+            Toggle = "CASIERS PORTATIFS",
+            Rows = " Lignes",
+            Cols = " Colonnes"
+        },
+        Description = {
+            Toggle = "Active la capacité de stockage personnalisée des Casiers Portatifs",
+            Rows = "Nombre de lignes à activer dans les Casiers Portatifs",
+            Cols = "Nombre de colonnes à activer dans les Casiers Portatifs"
+        }
+    },
+
+    TadpoleHaul = {
+        Title = {
+            Toggle = "CHÂSSIS FORT DU TÊTARD",
+            Rows = " Lignes",
+            Cols = " Colonnes"
+        },
+        Description = {
+            Toggle = "Active la capacité de stockage personnalisée du Châssis Fort",
+            Rows = "Nombre de lignes à activer dans le Châssis Fort",
+            Cols = "Nombre de colonnes à activer dans le Châssis Fort"
+        }
+    },
+
+    Bioreactor = {
+        Title = {
+            Toggle = "BIORÉACTEUR",
+            Rows = " Lignes",
+            Cols = " Colonnes"
+        },
+        Description = {
+            Toggle = "Active la capacité de stockage personnalisée du Bioréacteur",
+            Rows = "Nombre de lignes à activer dans le Bioréacteur",
+            Cols = "Nombre de colonnes à activer dans le Bioréacteur"
+        }
+    },
+
+    ProcessorStation = {
+        Title = {
+            Toggle = "STATION DE TRAITEMENT",
+            Rows = " Lignes",
+            Cols = " Colonnes"
+        },
+        Description = {
+            Toggle = "Active la capacité de stockage personnalisée de la Station de Traitement",
+            Rows = "Nombre de lignes d'Entrée/Sortie à activer dans la Station de Traitement",
+            Cols = "Nombre de colonnes d'Entrée/Sortie à activer dans la Station de Traitement"
+        }
+    },
+}
+
+return Translation
